9b7c0e5d457fc8d3c32cf158747cc8a88e15e0ca
Ali-Tamerr <extelx66@gmail.com>
2025-03-24 01:17:40 +0200

    first commit

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ command, mode }) => {
-  const isGitHub = process.env.GITHUB_ACTIONS === "true"; // Detect GitHub deployment
+  const isGitHub = process.env.GITHUB_ACTIONS === "true";
   return {
     plugins: [react()],
-    base: isGitHub ? "/nike-shoes-website-project/" : "/", // GitHub vs. Vercel
+    base: isGitHub ? "/nike-shoes-website-project/" : "/",
     server: {
       port: process.env.PORT || 5173,
     },

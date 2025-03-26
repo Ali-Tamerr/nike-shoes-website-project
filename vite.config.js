58b9bfc143a0a19106fcfa4978501b09dec248c2
Ali-Tamerr <extelx66@gmail.com>
2025-03-26 23:32:41 +0200

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vercel from 'vite-plugin-vercel';
 import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react(), vercel()],
-  base: process.env.VITE_BASE_PATH || "/nike-shoes-website-project/",
+  base: "/nike-shoes-website-project/",
   server: {
     port: process.env.PORT,
   },
